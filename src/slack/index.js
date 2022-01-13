02094ddf61de4154da1bb601940b053e9f7155c6
petercrackthecode <nguyenvandung02051999@gmail.com>
2022-01-13 15:32:43 -0800

    add token auth for the slack API

--- src/slack/index.js
+++ src/slack/index.js
@@ -1,8 +1,20 @@
 import express from "express"
 let slackRouter = express.Router()
+import * as slackUtils from "./utils"
 
 slackRouter.get("/port", (req, res) => {
   res.status(200).send(`PORT = ${process.env.PORT || "No Port found"}`)
 })
+
+slackRouter.get("/ServiceProviderConfigs", async (req, res) => {
+  const serviceProviderConfigs = await slackUtils.getServiceProviderConfigs()
+
+  console.log("serviceProvderConfigs = ", serviceProviderConfigs)
+  res.status(200).json({ serviceProviderConfigs })
+})
+
+// slackRouter.use((error, req, res, next) => {
+//   res.status(500).json({ error })
+// })
 
 export default slackRouter
--- src/slack/utils.js
+++ src/slack/utils.js
@@ -1,5 +1,14 @@
+import axios from "axios"
+import { config } from "dotenv"
+
+config()
 // The base URL for the Slack API SCIM
-const SLACK_SCIM_API_URL = "https://api.slack.com/scim/v1/"
+const SLACK_SCIM_API_URL = "https://api.slack.com/scim/v1"
+
+const authHeaders = {
+  Authorization: `Bearer ${process.env.SLACK_ORG_ADMIN_USER_TOKEN}`,
+  "Content-Type": "application/json",
+}
 
 // Limit (requests per minute): 300
 // Burst (requests): 300
@@ -32,3 +41,19 @@ export function updateGroup() {}
 // Limit (requests per minute): 180
 // Burst (requests): 20
 export function deleteGroup() {}
+
+export async function getServiceProviderConfigs() {
+  console.log("authHeaders = ", authHeaders)
+
+  const result = await axios({
+    method: "get",
+    url: `${SLACK_SCIM_API_URL}/ServiceProviderConfigs`,
+    headers: authHeaders,
+  }).then((res) => {
+    console.log(res.statusText)
+    console.log(res.status)
+    return res.data
+  })
+
+  return result
+}
