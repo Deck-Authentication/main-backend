f67bb7e86366fc382532acb49c1e66ea64fb8786
petercrackthecode <nguyenvandung02051999@gmail.com>
2022-01-14 21:02:49 -0800

    add input sanitizing through the isEmail function

--- src/slack/index.js
+++ src/slack/index.js
@@ -1,16 +1,19 @@
 import express from "express"
 let slackRouter = express.Router()
 import * as slackUtils from "./utils"
-
-slackRouter.get("/port", (req, res) => {
-  res.status(200).send(`PORT = ${process.env.PORT || "No Port found"}`)
-})
+// isEmail as a function from the main utils folder
+import { isEmail } from "../utils"
 
 /**
  * Get the user ID for a given email address, then get the user object for that ID.
  */
 slackRouter.get("/user/:email", async (req, res) => {
   const email = req.params.email
+  if (!isEmail(email))
+    res
+      .status(412)
+      .json({ message: "Error: input must be a valid email address" })
+
   const userId = await slackUtils.emailToUserId(email)
 
   const user = await slackUtils.getUser(userId)
@@ -18,16 +21,26 @@ slackRouter.get("/user/:email", async (req, res) => {
   res.status(200).json({ user })
 })
 
-slackRouter.get("/ServiceProviderConfigs", async (req, res) => {
+slackRouter.get("/service-provider-configs", async (req, res) => {
   const serviceProviderConfigs = await slackUtils.getServiceProviderConfigs()
 
   res.status(200).json({ serviceProviderConfigs })
 })
 
-slackRouter.get("/listConversations", async (req, res) => {
+slackRouter.get("/list-conversations", async (req, res) => {
   const conversations = await slackUtils.listConversations()
 
   res.status(200).json({ conversations })
+})
+
+slackRouter.put("/invite-to-channel", async (req, res) => {
+  const { email, channel } = req.body
+
+  const userId = await slackUtils.emailToUserId(email)
+
+  const invite = await slackUtils.inviteToChannel(userId, channel)
+
+  res.status(200).json({ invite })
 })
 
 // slackRouter.use((error, req, res, next) => {
