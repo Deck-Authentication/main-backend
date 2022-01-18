2c9f3c04dd444f6f9b5454c831a18d5827bc9c2a
petercrackthecode <nguyenvandung02051999@gmail.com>
2022-01-17 23:15:00 -0800

    Update the /github/invite-to-team route

--- src/github/index.js
+++ src/github/index.js
@@ -1,4 +1,4 @@
-import { Router } from "express"
+import { response, Router } from "express"
 import { Octokit } from "octokit"
 import { config } from "dotenv"
 // load environment variables from .env file
@@ -10,25 +10,45 @@ const octokit = new Octokit({
 })
 
 githubRouter.post("/invite-to-team", async (req, res) => {
-  const { org, team_slug, username, role } = req.body
-  await octokit
-    .request("PUT /orgs/:org/teams/:team_slug/memberships/:username", {
-      org,
-      team_slug,
-      username,
-      role,
+  const { org, team_slugs, users } = req.body
+
+  let invitations = []
+
+  team_slugs.map((team_slug) => {
+    users.map((user) =>
+      invitations.push(
+        octokit.request(
+          "PUT /orgs/:org/teams/:team_slug/memberships/:username",
+          {
+            org,
+            team_slug,
+            username: user.username,
+            role: user.role,
+          }
+        )
+      )
+    )
+  })
+
+  await Promise.all(invitations)
+    .then((responses) => {
+      responses.map((response) => {
+        if (response.status != 200)
+          res.status(500).json({
+            message: `Request failed`,
+            responseStatus: response.status,
+          })
+      })
+
+      res.status(200).json({
+        message: `Invite successfully`,
+      })
     })
-    .then((response) => {
-      if (response.status == 200)
-        res.status(200).json({
-          message: `Successfully invite ${username} to ${team_slug} with role ${role}`,
-        })
-      else
-        res.status(500).json({
-          message: `Request failed with the Github status ${response.status}`,
-        })
-    })
-    .catch((err) => res.status(500).json({ message: err.message }))
+    .catch((err) =>
+      res
+        .status(500)
+        .json({ message: "Request failed from Github", error: err })
+    )
 })
 
 githubRouter.delete("/remove-from-team", async (req, res) => {
