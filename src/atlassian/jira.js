fdf29943f1ea856756df3c9b8d24255ff3120800
petercrackthecode <nguyenvandung02051999@gmail.com>
2022-01-18 16:16:37 -0800

    Add a jira client to evoke API calls.

--- src/atlassian/jira.js
+++ src/atlassian/jira.js
@@ -1,13 +1,39 @@
 import { Router } from "express"
+import { config } from "dotenv"
+import { Version2Client } from "jira.js"
+
+// load environment variables from .env file
+config()
+
+const jiraClient = new Version2Client({
+  host: process.env.ATLASSIAN_HOST,
+  authentication: {
+    basic: {
+      email: process.env.ATLASSIAN_EMAIL,
+      apiToken: process.env.ATLASSIAN_API_TOKEN,
+    },
+  },
+})
 
 const jiraRouter = Router()
 
-jiraRouter.post("/create-team", (req, res) => {})
+jiraRouter.post("/create-team", async (req, res) => {})
 
-jiraRouter.delete("/delete-team", (req, res) => {})
+jiraRouter.delete("/delete-team", async (req, res) => {})
 
-jiraRouter.post("/invite-to-team", (req, res) => {})
+jiraRouter.post("/invite-to-team", async (req, res) => {})
 
-jiraRouter.delete("/remove-from-team", (req, res) => {})
+jiraRouter.delete("/remove-from-team", async (req, res) => {})
+
+// This route is for testing purposes only
+jiraRouter.get("/get-projects", async (req, res) => {
+  const projects = await jiraClient.projects
+    .getAllProjects()
+    .catch((err) => res.status(500).json(err))
+
+  console.log(projects)
+
+  res.status(200).json(projects)
+})
 
 export default jiraRouter
