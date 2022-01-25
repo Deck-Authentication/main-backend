f5441fb8feb200fc161c98d485e0c893b79b892c
petercrackthecode <nguyenvandung02051999@gmail.com>
2022-01-24 19:39:14 -0800

    add the template/get-template-by-id route

--- src/template/index.js
+++ src/template/index.js
@@ -6,8 +6,27 @@ const templateRouter = Router()
 templateRouter.get("/get-all-template", async (_, res) => {
   await Template.find({})
     .exec()
-    .then((template) => res.status(200).json({ template }))
-    .catch((err) => res.status(500).json({ message: err }))
+    .then((template) => res.status(200).json({ template, ok: true }))
+    .catch((err) => res.status(500).json({ message: err, ok: false }))
+})
+
+templateRouter.get("/get-template-by-id", async (req, res) => {
+  if (!req.params.id)
+    res.status(400).json({ message: "id is required", ok: false })
+
+  await Template.findById(req.params.id)
+    .exec()
+    .then((template) => {
+      if (!template)
+        res
+          .status(404)
+          .json({
+            message: "no template found matched the provided id",
+            ok: true,
+          })
+      else res.status(200).json({ template, ok: true })
+    })
+    .catch((err) => res.status(500).json({ message: err, ok: false }))
 })
 
 templateRouter.post("/create-team", (req, res) => {})
