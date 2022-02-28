9ad34845cd221ddfbc9c1074ce6721de54603573
petercrackthecode <nguyenvandung02051999@gmail.com>
2022-02-27 22:31:26 -0800

    add helmet and cors rules to secure the backend

--- src/app.js
+++ src/app.js
@@ -6,6 +6,7 @@ import atlassianRouter from "./integrations/atlassian"
 import templateRouter from "./template"
 import userRouter from "./users"
 import cors from "cors"
+import helmet from "helmet"
 import { connectDB } from "./database"
 const app = express()
 
@@ -15,12 +16,22 @@ const port = process.env.PORT || 1999
 
 connectDB.call(this)
 
-app.use(cors())
+// in production environment, only allow requests from the frontend: https://app.withdeck.com
+const corsOptions =
+    process.env.ENVIRONMENT === "production" ?
+    {
+        origin: "https://app.withdeck.com",
+        optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+    } :
+    {}
+
+app.use(cors(corsOptions))
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
+app.use(helmet())
 
 app.get("/", (_, res) => {
-  res.send("Hello World!")
+    res.send("Hello World!")
 })
 
 // templates
@@ -36,5 +47,5 @@ app.use("/github", githubRouter)
 app.use("/atlassian", atlassianRouter)
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+    console.log(`Example app listening at http://localhost:${port}`)
 })
