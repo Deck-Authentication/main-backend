c68f0bd48cd634db280f5995cb067d8c91378a66
petercrackthecode <nguyenvandung02051999@gmail.com>
2022-03-07 19:34:19 -0800

    Small changes

--- src/app.js
+++ src/app.js
@@ -25,12 +25,12 @@ const corsOptions =
     } :
     {}
 
-app.use(cors(corsOptions))
+app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use(helmet())
 
-app.get("/", (_, res) => {
+app.get("/", cors(), (_, res) => {
     res.send("Hello World!")
 })
 
